0d0a0315d58c85d526170f111194e5651c707e3c
unknown <studentath@student.ubb.edu.pl>
2025-03-16 16:06:26 +0100

    Pierwszy commit

---
+++ Programowanie_V/MainWindow.xaml.cs
@@ -0,0 +1,182 @@
+﻿using OxyPlot.Series;
+using OxyPlot.Wpf;
+using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace Programowanie_V
+{
+    public partial class MainWindow : Window
+    {
+        private Przebiegi trojkat;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            trojkat = new Przebiegi();
+        }
+
+        private void btnWykresTrojkat_Click(object sender, RoutedEventArgs e)
+        {
+            // Generujemy przebieg trójkątny
+            trojkat.Generuj_Trojkat(amplituda: 1.0, okres: 2.0, krok: 0.1, t_trwania: 10.0);
+
+            // Tworzymy model wykresu
+            var model = new PlotModel { Title = "Przebieg trójkątny" };
+
+            // Dodajemy siatkę
+            model.Axes.Add(new OxyPlot.Axes.LinearAxis
+            {
+                Position = OxyPlot.Axes.AxisPosition.Left,
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dot,
+                MajorGridlineColor = OxyColors.Gray,
+                MinorGridlineColor = OxyColors.LightGray,
+                Minimum = -1,
+                Maximum = 1,
+            });
+
+            model.Axes.Add(new OxyPlot.Axes.LinearAxis
+            {
+                Position = OxyPlot.Axes.AxisPosition.Bottom,
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dot,
+                MajorGridlineColor = OxyColors.Gray,
+                MinorGridlineColor = OxyColors.LightGray,
+               // Minimum = 0,
+               // Maximum = 10,
+            });
+
+            // Tworzymy serię i dodajemy punkty
+            var seria = new LineSeries { Title = "Trojkat", Color = OxyColors.Blue };
+
+            for (int i = 0; i < trojkat.xTrojkat.Count; i++)
+            {
+                seria.Points.Add(new DataPoint(trojkat.xTrojkat[i], trojkat.yTrojkat[i]));
+            }
+
+            // Dodajemy serię do modelu
+            model.Series.Add(seria);
+            RysujFFT(trojkat.yTrojkat);
+            // Przypisujemy model do PlotView
+            plotview.Model = model;
+        }
+
+        private void btn_Wykres_Sin_Click(object sender, RoutedEventArgs e)
+        {
+            // Generujemy przebieg sinusoidalny
+            trojkat.Generuj_Sinus(amplituda: 1.0, czestotliwosc: 0.5, krok: 0.1, t_trwania: 10.0);
+
+            // Tworzymy model wykresu
+            var model = new PlotModel { Title = "Przebieg sinusoidalny" };
+
+            // Dodajemy siatkę
+            model.Axes.Add(new OxyPlot.Axes.LinearAxis
+            {
+                Position = OxyPlot.Axes.AxisPosition.Left,
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dot,
+                MajorGridlineColor = OxyColors.Gray,
+                MinorGridlineColor = OxyColors.LightGray,
+                //Minimum = -1,
+                //Maximum = 1,
+            });
+
+            model.Axes.Add(new OxyPlot.Axes.LinearAxis
+            {
+                Position = OxyPlot.Axes.AxisPosition.Bottom,
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dot,
+                MajorGridlineColor = OxyColors.Gray,
+                MinorGridlineColor = OxyColors.LightGray,
+                //Minimum = 0,
+                //Maximum = 10,
+            });
+
+            // Tworzymy serię i dodajemy punkty
+            var seriaSin = new LineSeries { Title = "Sinus", Color = OxyColors.Green };
+
+            for (int i = 0; i < trojkat.xSin.Count; i++)
+            {
+                seriaSin.Points.Add(new DataPoint(trojkat.xSin[i], trojkat.ySin[i]));
+            }
+
+            // Dodajemy serię do modelu
+            model.Series.Add(seriaSin);
+
+            // Przypisujemy model do PlotView
+            plotview.Model = model;
+            RysujFFT(trojkat.ySin);
+        }
+
+        private void btn_wykres_prostokat_Click(object sender, RoutedEventArgs e)
+        {
+            // Generujemy przebieg prostokątny
+            trojkat.Generuj_Prostokat(amplituda: 1.0, czestotliwosc: 0.5, krok: 0.1, t_trwania: 10.0);
+
+            // Tworzymy model wykresu
+            var model = new PlotModel { Title = "Przebieg prostokątny" };
+
+            // Dodajemy siatkę
+            model.Axes.Add(new OxyPlot.Axes.LinearAxis
+            {
+                Position = OxyPlot.Axes.AxisPosition.Left,
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dot,
+                MajorGridlineColor = OxyColors.Gray,
+                MinorGridlineColor = OxyColors.LightGray,
+                Minimum = -1,
+                Maximum = 1,
+            });
+
+            model.Axes.Add(new OxyPlot.Axes.LinearAxis
+            {
+                Position = OxyPlot.Axes.AxisPosition.Bottom,
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dot,
+                MajorGridlineColor = OxyColors.Gray,
+                MinorGridlineColor = OxyColors.LightGray,
+                Minimum = 0,
+                Maximum = 10,
+            });
+
+            // Tworzymy serię i dodajemy punkty
+            var seriaProstokat = new LineSeries { Title = "Prostokat", Color = OxyColors.Red };
+
+            for (int i = 0; i < trojkat.xProstokat.Count; i++)
+            {
+                seriaProstokat.Points.Add(new DataPoint(trojkat.xProstokat[i], trojkat.yProstokat[i]));
+            }
+
+            // Dodajemy serię do modelu
+            model.Series.Add(seriaProstokat);
+
+            RysujFFT(trojkat.yProstokat);
+            // Przypisujemy model do PlotView
+            plotview.Model = model;
+        }
+
+
+        private void RysujFFT(List<double> sygnal)
+        {
+            // Obliczamy FFT dla danego sygnału
+            double czestotliwoscProbkowania = 1.0 / 0.1; // Zakładając, że krok to 0.1 (czas próbkowania)
+            var wynikFFT = trojkat.ObliczFFT(sygnal, czestotliwoscProbkowania);
+
+            // Tworzymy model wykresu FFT
+            var modelFFT = new PlotModel { Title = "Transformata Fouriera" };
+            var seriaFFT = new LineSeries { Title = "FFT", Color = OxyColors.Purple };
+
+            for (int i = 0; i < wynikFFT.Item1.Count; i++)
+            {
+                seriaFFT.Points.Add(new DataPoint(wynikFFT.Item1[i], wynikFFT.Item2[i]));
+            }
+
+            modelFFT.Series.Add(seriaFFT);
+            plotview_FFT.Model = modelFFT;
+        }
+
+
+    }
+}
---
+++ Programowanie_V/Przebiegi.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using MathNet.Numerics.IntegralTransforms;
+using System.Numerics;
+
+
+
+namespace Programowanie_V
+{
+    internal class Przebiegi
+    {
+        public List<double> xTrojkat { get; set; }
+        public List<double> yTrojkat { get; set; }
+        public List<double> xSin { get; set; }
+        public List<double> ySin { get; set; }
+        public List<double> xProstokat { get; set; }
+        public List<double> yProstokat { get; set; }
+
+        public Przebiegi()
+        {
+            xTrojkat = new List<double>();
+            yTrojkat = new List<double>();
+
+            xSin = new List<double>();
+            ySin = new List<double>();
+
+            xProstokat = new List<double>();
+            yProstokat = new List<double>();
+        }
+
+        public void Generuj_Trojkat(double amplituda, double okres, double krok, double t_trwania)
+        {
+            xTrojkat.Clear();
+            yTrojkat.Clear();
+
+            for (double t = 0; t <= t_trwania; t += krok)
+            {
+                double y = amplituda * (4 * Math.Abs((t / okres) % 1 - 0.5) - 1);
+                xTrojkat.Add(t);
+                yTrojkat.Add(y);
+            }
+        }
+
+        public void Generuj_Sinus(double amplituda, double czestotliwosc, double krok, double t_trwania)
+        {
+            xSin.Clear();
+            ySin.Clear();
+
+            for (double t = 0; t <= t_trwania; t += krok)
+            {
+                double y = amplituda * Math.Sin(2 * Math.PI * czestotliwosc * t);
+                xSin.Add(t);
+                ySin.Add(y);
+            }
+        }
+
+        public void Generuj_Prostokat(double amplituda, double czestotliwosc, double krok, double t_trwania)
+        {
+            xProstokat.Clear();
+            yProstokat.Clear();
+
+            for (double t = 0; t <= t_trwania; t += krok)
+            {
+                double y = amplituda * Math.Sign(Math.Sin(2 * Math.PI * czestotliwosc * t));
+                xProstokat.Add(t);
+                yProstokat.Add(y);
+            }
+        }
+
+        public (List<double>, List<double>) ObliczFFT(List<double> sygnal, double czestotliwoscProbkowania)
+        {
+            int N = sygnal.Count;
+            Complex[] daneFFT = new Complex[N];
+            for (int i = 0; i < N; i++)
+            {
+                daneFFT[i] = new Complex(sygnal[i], 0);
+            }
+
+            Fourier.Forward(daneFFT, FourierOptions.Matlab);
+
+            List<double> amplitudy = new List<double>();
+            List<double> czestotliwosci = new List<double>();
+
+            for (int i = 0; i < N / 2; i++) // Analizujemy tylko pierwszą połowę widma
+            {
+                double freq = (double)i * czestotliwoscProbkowania / N;
+                double amp = daneFFT[i].Magnitude;
+
+                czestotliwosci.Add(freq);
+                amplitudy.Add(amp);
+            }
+
+            return (czestotliwosci, amplitudy);
+        }
+    }
+}
